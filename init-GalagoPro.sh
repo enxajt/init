f8d3a65b2454db8e8cd4dbc553fadaed5b19779c
enxajt <enxajt@gmail.com>
2017-07-11 13:50:58 +0900

    sth ? and i3wm

--- init-GalagoPro.sh
+++ init-GalagoPro.sh
@@ -7,20 +7,22 @@ sudo chsh -s /bin/zsh
 
 
 # wine
-sudo dpkg --add-architecture i386 
-wget -nc https://dl.winehq.org/wine-builds/Release.key
-sudo apt-key add Release.key
-sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
-sudo apt-get update
-sudo apt-get install -y --install-recommends winehq-stable
+sudo dpkg --add-architecture i386 \
+ && wget -nc https://dl.winehq.org/wine-builds/Release.key \
+ && sudo apt-key add Release.key \
+ && sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/ \
+ && sudo apt-get update \
+ && sudo apt-get install -y --install-recommends winehq-stable \
+ && sudo apt-get install flex bison \
+ && sudo apt-get install -y gcc-multilib libntlm0 libx11-dev:i386 libfreetype6-dev:i386
 
-sudo apt-get install flex bison
-sudo apt-get install -y gcc-multilib libntlm0 libx11-dev:i386 libfreetype6-dev:i386
-sudo apt-get install -y wine.i686
+# && sudo apt-get install -y wine.i686 \
+sudo apt-get install cabextract \
+ && cd ~/wine/ \
+ && sh winetricks allfonts \
+ && sh winetricks fontsmooth-rgb
 
-sudo apt-get install cabextract
-sh winetricks allfonts
-sh winetricks fontsmooth-rgb
+
 
 # safe rm
 cd ~/enx-init/DL/ \
