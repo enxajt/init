9b0718907946e2a6130f1f36aa2c842c702d06bc
enxajt <enxajt@gmail.com>
2017-08-04 07:08:09 +0900

    sth

---
+++ install-tmux.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -xeu
+
+sudo apt-get install -y automake libevent-dev libncurses5-dev xsel
+
+if [ ! -d tmux ]; then
+    git clone https://github.com/tmux/tmux.git
+fi
+
+cd tmux
+
+# checkout latest tag
+#git checkout 2.2
+git checkout $(git tag | sort -V | tail -n 1)
+
+sh autogen.sh
+
+./configure
+
+make -j4
+
+sudo make install
