3dfc9768ae805c4ddcecb9f43270ea71b6139749
enxajt <enxajt@gmail.com>
2018-02-06 13:01:51 +0900

    moved from playbook

--- install-vagrant.sh
+++ install-vagrant.sh
@@ -8,3 +8,10 @@ sudo apt-get install -y virtualbox
 
 wget https://releases.hashicorp.com/vagrant/1.9.5/vagrant_1.9.5_x86_64.deb
 sudo dpkg --install vagrant_1.9.5_x86_64.deb
+
+sudo mkdir -p /vagrant/shared/bak/apt
+sudo [ -e /vagrant/shared/bak/apt/archives ] && mv /vagrant/shared/bak/apt/archives /vagrant/shared/bak/apt/archives.bak
+sudo cp -r /var/cache/apt/archives/ /vagrant/shared/bak/apt/
+sudo mv /vagrant/shared/bak/apt/pkgcache /vagrant/shared/bak/apt/pkgcache.bak
+sudo cp -r /var/cache/apt/pkgcache.bin /vagrant/shared/bak/apt/
+sudo apt-get clean
